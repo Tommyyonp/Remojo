a7a14babeca49c422653a8b4fcf4968590bdd1b4
Tommy Yon Prakoso <tommyyonprakoso@Tommys-MacBook-Air.local>
2022-10-20 15:43:23 +0700

    add files

---
+++ src/components/ProductCard.vue
@@ -0,0 +1,18 @@
+<template>
+        <div class="w-full md:w-1/3 pt-16 px-6 flex flex-col flex-grow flex-shrink rounded-2xl">
+          <div class="flex-1 bg-white rounded-2xl overflow-hidden shadow">
+            <img src="../assets/images/Beat.jpeg" alt="">
+            <a href="" class="flex flex-wrap no-underline hover:no-underline">
+              <div class="w-full font-bold text-xl text-at-blue px-6 py-2">
+                Honda Beat 2022
+              </div>
+              <p class="text-at-green text-base px-6 mb-5">
+                Rp. 500.000
+              </p>
+            </a>
+          </div>
+        </div>    
+</template>
+<script>
+
+</script>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -44,45 +44,9 @@
         <h2 class="w-full my-2 text-5xl font-bold leading-tight text-center text-at-blue">
           Catalogue
         </h2>
-        <div class="w-full md:w-1/3 pt-16 px-6 flex flex-col flex-grow flex-shrink rounded-2xl">
-          <div class="flex-1 bg-white rounded-2xl overflow-hidden shadow">
-            <img src="../assets/images/Beat.jpeg" alt="">
-            <a href="" class="flex flex-wrap no-underline hover:no-underline">
-              <div class="w-full font-bold text-xl text-at-blue px-6 py-2">
-                Honda Beat 2022
-              </div>
-              <p class="text-at-green text-base px-6 mb-5">
-                Rp. 500.000
-              </p>
-            </a>
-          </div>
-        </div>
-        <div class="w-full md:w-1/3 pt-16 px-6 flex flex-col flex-grow flex-shrink rounded-2xl">
-          <div class="flex-1 bg-white rounded-2xl overflow-hidden shadow">
-            <img src="../assets/images/Beat.jpeg" alt="">
-            <a href="" class="flex flex-wrap no-underline hover:no-underline">
-              <div class="w-full font-bold text-xl text-at-blue px-6 py-2">
-                Honda Beat 2022
-              </div>
-              <p class="text-at-green text-base px-6 mb-5">
-                Rp. 500.000
-              </p>
-            </a>
-          </div>
-        </div>
-        <div class="w-full md:w-1/3 pt-16 px-6 flex flex-col flex-grow flex-shrink rounded-2xl">
-          <div class="flex-1 bg-white rounded-2xl overflow-hidden shadow">
-            <img src="../assets/images/Beat.jpeg" alt="">
-            <a href="" class="flex flex-wrap no-underline hover:no-underline">
-              <div class="w-full font-bold text-xl text-at-blue px-6 py-2">
-                Honda Beat 2022
-              </div>
-              <p class="text-at-green text-base px-6 mb-5">
-                Rp. 500.000
-              </p>
-            </a>
-          </div>
-        </div>
+        <ProductCard />
+        <ProductCard />
+        <ProductCard />
       </div>
     </section>
 
@@ -114,9 +78,10 @@
 <script>
 import { ref } from "vue";
 import { supabase } from "../supabase/init";
+import ProductCard from "../components/ProductCard.vue";
 export default {
   name: "home",
-  components: {},
+  components: { ProductCard },
   setup() {
     // Create data / vars
     const data = ref([]);
